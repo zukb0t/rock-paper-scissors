48e6e0b0c5d1d6d6d8917359836966f393faca3e
Duong Tang <dttang1994@gmail.com>
2024-05-20 20:19:50 -0700

    made some adjustments.

--- javascript.js
+++ javascript.js
@@ -2,27 +2,32 @@ let humanScore = 0;
 let computerScore = 0;
 
 
+
+//0 is rock, 1 is paper, 2 is scissors
+
+//paper beats rock 1 > 0
+//scissors beat paper 2 > 1
+//rock beats scissors 0 < 2
+
+
 function playRound(humanChoice, robotChoice){
-
-}
-
-function getComputerChoice(){
-    let max = Math.floor(Math.random() * 3);
-    if(max === 0){
-        return "Rock";
-    }
-    else if(max === 1){
-        return "Paper";
-    }
-    else{
-        return "Scissors";
+    if (humanChoice === robotChoice){
+        console.log("draw");
     }
 }
+
+const getComputerChoice = function(){
+    return Math.floor(Math.random() * 3);
+};
 
 function getHumanChoice(){
-    let choice = prompt("0:Rock, 1:Paper, 2:Scissors");
-}
+    let choice = prompt("Welcome to a game of Rock, Paper," +
+    "Scissors. Pick a value between 0-2, where 0 is rock, 1 is paper, 2 is scissors.");
+    return Number(choice);
+};
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+
+for(let i = 0; i < 6; i++){
+    playRound(getHumanChoice(), getComputerChoice());
+}
 
